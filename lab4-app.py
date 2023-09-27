2651d87b486e1f27775300ee08a3bf3374d037d9
joncoe <info@joncoe.com>
2023-09-27 17:00:07 -0400

    Add /test route that prints the received data and returns a success JSON

--- lab4-app.py
+++ lab4-app.py
@@ -12,6 +12,19 @@ def home():
         data = request.get_json()
         return jsonify(data)
 
+@app.route('/test', methods=['POST'])
+def test():
+    if request.method == 'POST':
+        data_id = request.form.get('Id')
+        data_customer = request.form.get('Customer')
+        data_quantity = request.form.get('Quantity')
+        data_price = request.form.get('Price')
+        print('Id: ', data_id)
+        print('Customer', data_customer)
+        print('Quantity', data_quantity)
+        print('Price', data_price)
+        return jsonify({'data': 'success'})
+
 @app.route("/home/<int:num>", methods=["GET"])
 def disp(num):
     return jsonify({'data': num * 2})
