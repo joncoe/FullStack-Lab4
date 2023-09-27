f20e1a69c4ebd3bd5fd53375dc350d7beb9f70e5
joncoe <info@joncoe.com>
2023-09-27 15:28:23 -0400

    Lab step 1

---
+++ lab4-app.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "GET":
+        data = "hello world"
+        return jsonify({"data": data})
+
+
+@app.route("/home/<int:num>", methods=["GET"])
+def disp(num):
+    return jasonify({data: num * 2})
+
+app.run()
